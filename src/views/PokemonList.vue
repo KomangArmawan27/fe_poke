444d8f15cf0b86a3c62a618f286688e743783bc5
KomangArmawan <komang.armawan.27@gmail.com>
2025-05-25 15:06:12 +0700

    update: fetching data through pokeAPI and implement pagination

--- src/views/PokemonList.vue
+++ src/views/PokemonList.vue
@@ -1,203 +1,61 @@
 <template>
   <MainLayout>
     <h2 class="text-2xl font-semibold mb-4">Pokemon List</h2>
+    
+    <!-- Grid -->
     <div class="p-4 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        <div class="bg-white hover:bg-gray-100 transition transform active:scale-95 duration-150 ease-in-out cursor-pointer rounded shadow-lg h-48">
-            <router-link to="/pokemon-add">
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/27.png" alt="Pokemon Placeholder" class="w-full h-3/4 object-contain ">
-                <p class="text-center mt-2">Pokemon Placeholder</p>
-            </router-link>
-        </div>
-        <div class="bg-white hover:bg-gray-100 transition transform active:scale-95 duration-150 ease-in-out cursor-pointer rounded shadow-lg h-48">
-            <router-link to="/pokemon-add">
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/26.png" alt="Pokemon Placeholder" class="w-full h-3/4 object-contain ">
-                <p class="text-center mt-2">Pokemon Placeholder</p>
-            </router-link>
-        </div>
-        <div class="bg-white hover:bg-gray-100 transition transform active:scale-95 duration-150 ease-in-out cursor-pointer rounded shadow-lg h-48">
-            <router-link to="/pokemon-add">
-                <img src="https://cdn.pixabay.com/photo/2021/12/26/17/31/pokemon-list-6895600_640.png" alt="Pokemon Placeholder" class="w-full h-3/4 object-contain ">
-                <p class="text-center mt-2">Pokemon Placeholder</p>
-            </router-link>
-        </div>
-        <div class="bg-white hover:bg-gray-100 transition transform active:scale-95 duration-150 ease-in-out cursor-pointer rounded shadow-lg h-48">
-            <router-link to="/pokemon-add">
-                <img src="https://cdn.pixabay.com/photo/2021/12/26/17/31/pokemon-list-6895600_640.png" alt="Pokemon Placeholder" class="w-full h-3/4 object-contain ">
-                <p class="text-center mt-2">Pokemon Placeholder</p>
-            </router-link>
-        </div>
-        <div class="bg-white hover:bg-gray-100 transition transform active:scale-95 duration-150 ease-in-out cursor-pointer rounded shadow-lg h-48">
-            <router-link to="/pokemon-add">
-                <img src="https://cdn.pixabay.com/photo/2021/12/26/17/31/pokemon-list-6895600_640.png" alt="Pokemon Placeholder" class="w-full h-3/4 object-contain ">
-                <p class="text-center mt-2">Pokemon Placeholder</p>
-            </router-link>
-        </div>
-        <div class="bg-white hover:bg-gray-100 transition transform active:scale-95 duration-150 ease-in-out cursor-pointer rounded shadow-lg h-48">
-            <router-link to="/pokemon-add">
-                <img src="https://cdn.pixabay.com/photo/2021/12/26/17/31/pokemon-list-6895600_640.png" alt="Pokemon Placeholder" class="w-full h-3/4 object-contain ">
-                <p class="text-center mt-2">Pokemon Placeholder</p>
-            </router-link>
-        </div>
-        <div class="bg-white hover:bg-gray-100 transition transform active:scale-95 duration-150 ease-in-out cursor-pointer rounded shadow-lg h-48">
-            <router-link to="/pokemon-add">
-                <img src="https://cdn.pixabay.com/photo/2021/12/26/17/31/pokemon-list-6895600_640.png" alt="Pokemon Placeholder" class="w-full h-3/4 object-contain ">
-                <p class="text-center mt-2">Pokemon Placeholder</p>
-            </router-link>
-        </div>
-        <div class="bg-white hover:bg-gray-100 transition transform active:scale-95 duration-150 ease-in-out cursor-pointer rounded shadow-lg h-48">
-            <router-link to="/pokemon-add">
-                <img src="https://cdn.pixabay.com/photo/2021/12/26/17/31/pokemon-list-6895600_640.png" alt="Pokemon Placeholder" class="w-full h-3/4 object-contain ">
-                <p class="text-center mt-2">Pokemon Placeholder</p>
-            </router-link>
-        </div>
-        <div class="bg-white hover:bg-gray-100 transition transform active:scale-95 duration-150 ease-in-out cursor-pointer rounded shadow-lg h-48">
-            <router-link to="/pokemon-add">
-                <img src="https://cdn.pixabay.com/photo/2021/12/26/17/31/pokemon-list-6895600_640.png" alt="Pokemon Placeholder" class="w-full h-3/4 object-contain ">
-                <p class="text-center mt-2">Pokemon Placeholder</p>
-            </router-link>
-        </div>
-        <div class="bg-white hover:bg-gray-100 transition transform active:scale-95 duration-150 ease-in-out cursor-pointer rounded shadow-lg h-48">
-            <router-link to="/pokemon-add">
-                <img src="https://cdn.pixabay.com/photo/2021/12/26/17/31/pokemon-list-6895600_640.png" alt="Pokemon Placeholder" class="w-full h-3/4 object-contain ">
-                <p class="text-center mt-2">Pokemon Placeholder</p>
-            </router-link>
-        </div>
-        <div class="bg-white hover:bg-gray-100 transition transform active:scale-95 duration-150 ease-in-out cursor-pointer rounded shadow-lg h-48">
-            <router-link to="/pokemon-add">
-                <img src="https://cdn.pixabay.com/photo/2021/12/26/17/31/pokemon-list-6895600_640.png" alt="Pokemon Placeholder" class="w-full h-3/4 object-contain ">
-                <p class="text-center mt-2">Pokemon Placeholder</p>
-            </router-link>
-        </div>
-        <div class="bg-white hover:bg-gray-100 transition transform active:scale-95 duration-150 ease-in-out cursor-pointer rounded shadow-lg h-48">
-            <router-link to="/pokemon-add">
-                <img src="https://cdn.pixabay.com/photo/2021/12/26/17/31/pokemon-list-6895600_640.png" alt="Pokemon Placeholder" class="w-full h-3/4 object-contain ">
-                <p class="text-center mt-2">Pokemon Placeholder</p>
-            </router-link>
-        </div>
-        <div class="bg-white hover:bg-gray-100 transition transform active:scale-95 duration-150 ease-in-out cursor-pointer rounded shadow-lg h-48">
-            <router-link to="/pokemon-add">
-                <img src="https://cdn.pixabay.com/photo/2021/12/26/17/31/pokemon-list-6895600_640.png" alt="Pokemon Placeholder" class="w-full h-3/4 object-contain ">
-                <p class="text-center mt-2">Pokemon Placeholder</p>
-            </router-link>
-        </div>
-        <div class="bg-white hover:bg-gray-100 transition transform active:scale-95 duration-150 ease-in-out cursor-pointer rounded shadow-lg h-48">
-            <router-link to="/pokemon-add">
-                <img src="https://cdn.pixabay.com/photo/2021/12/26/17/31/pokemon-list-6895600_640.png" alt="Pokemon Placeholder" class="w-full h-3/4 object-contain ">
-                <p class="text-center mt-2">Pokemon Placeholder</p>
-            </router-link>
-        </div>
-        <div class="bg-white hover:bg-gray-100 transition transform active:scale-95 duration-150 ease-in-out cursor-pointer rounded shadow-lg h-48">
-            <router-link to="/pokemon-add">
-                <img src="https://cdn.pixabay.com/photo/2021/12/26/17/31/pokemon-list-6895600_640.png" alt="Pokemon Placeholder" class="w-full h-3/4 object-contain ">
-                <p class="text-center mt-2">Pokemon Placeholder</p>
-            </router-link>
-        </div>
-        <div class="bg-white hover:bg-gray-100 transition transform active:scale-95 duration-150 ease-in-out cursor-pointer rounded shadow-lg h-48">
-            <router-link to="/pokemon-add">
-                <img src="https://cdn.pixabay.com/photo/2021/12/26/17/31/pokemon-list-6895600_640.png" alt="Pokemon Placeholder" class="w-full h-3/4 object-contain ">
-                <p class="text-center mt-2">Pokemon Placeholder</p>
-            </router-link>
-        </div>
-        <div class="bg-white hover:bg-gray-100 transition transform active:scale-95 duration-150 ease-in-out cursor-pointer rounded shadow-lg h-48">
-            <router-link to="/pokemon-add">
-                <img src="https://cdn.pixabay.com/photo/2021/12/26/17/31/pokemon-list-6895600_640.png" alt="Pokemon Placeholder" class="w-full h-3/4 object-contain ">
-                <p class="text-center mt-2">Pokemon Placeholder</p>
-            </router-link>
-        </div>
-        <div class="bg-white hover:bg-gray-100 transition transform active:scale-95 duration-150 ease-in-out cursor-pointer rounded shadow-lg h-48">
-            <router-link to="/pokemon-add">
-                <img src="https://cdn.pixabay.com/photo/2021/12/26/17/31/pokemon-list-6895600_640.png" alt="Pokemon Placeholder" class="w-full h-3/4 object-contain ">
-                <p class="text-center mt-2">Pokemon Placeholder</p>
-            </router-link>
-        </div>
-        <div class="bg-white hover:bg-gray-100 transition transform active:scale-95 duration-150 ease-in-out cursor-pointer rounded shadow-lg h-48">
-            <router-link to="/pokemon-add">
-                <img src="https://cdn.pixabay.com/photo/2021/12/26/17/31/pokemon-list-6895600_640.png" alt="Pokemon Placeholder" class="w-full h-3/4 object-contain ">
-                <p class="text-center mt-2">Pokemon Placeholder</p>
-            </router-link>
-        </div>
-        <div class="bg-white hover:bg-gray-100 transition transform active:scale-95 duration-150 ease-in-out cursor-pointer rounded shadow-lg h-48">
-            <router-link to="/pokemon-add">
-                <img src="https://cdn.pixabay.com/photo/2021/12/26/17/31/pokemon-list-6895600_640.png" alt="Pokemon Placeholder" class="w-full h-3/4 object-contain ">
-                <p class="text-center mt-2">Pokemon Placeholder</p>
-            </router-link>
-        </div>
-        <div class="bg-white hover:bg-gray-100 transition transform active:scale-95 duration-150 ease-in-out cursor-pointer rounded shadow-lg h-48">
-            <router-link to="/pokemon-add">
-                <img src="https://cdn.pixabay.com/photo/2021/12/26/17/31/pokemon-list-6895600_640.png" alt="Pokemon Placeholder" class="w-full h-3/4 object-contain ">
-                <p class="text-center mt-2">Pokemon Placeholder</p>
-            </router-link>
-        </div>
-        <div class="bg-white hover:bg-gray-100 transition transform active:scale-95 duration-150 ease-in-out cursor-pointer rounded shadow-lg h-48">
-            <router-link to="/pokemon-add">
-                <img src="https://cdn.pixabay.com/photo/2021/12/26/17/31/pokemon-list-6895600_640.png" alt="Pokemon Placeholder" class="w-full h-3/4 object-contain ">
-                <p class="text-center mt-2">Pokemon Placeholder</p>
-            </router-link>
-        </div>
-        <div class="bg-white hover:bg-gray-100 transition transform active:scale-95 duration-150 ease-in-out cursor-pointer rounded shadow-lg h-48">
-            <router-link to="/pokemon-add">
-                <img src="https://cdn.pixabay.com/photo/2021/12/26/17/31/pokemon-list-6895600_640.png" alt="Pokemon Placeholder" class="w-full h-3/4 object-contain ">
-                <p class="text-center mt-2">Pokemon Placeholder</p>
-            </router-link>
-        </div>
-        <div class="bg-white hover:bg-gray-100 transition transform active:scale-95 duration-150 ease-in-out cursor-pointer rounded shadow-lg h-48">
-            <router-link to="/pokemon-add">
-                <img src="https://cdn.pixabay.com/photo/2021/12/26/17/31/pokemon-list-6895600_640.png" alt="Pokemon Placeholder" class="w-full h-3/4 object-contain ">
-                <p class="text-center mt-2">Pokemon Placeholder</p>
-            </router-link>
-        </div>
-        <div class="bg-white hover:bg-gray-100 transition transform active:scale-95 duration-150 ease-in-out cursor-pointer rounded shadow-lg h-48">
-            <router-link to="/pokemon-add">
-                <img src="https://cdn.pixabay.com/photo/2021/12/26/17/31/pokemon-list-6895600_640.png" alt="Pokemon Placeholder" class="w-full h-3/4 object-contain ">
-                <p class="text-center mt-2">Pokemon Placeholder</p>
-            </router-link>
-        </div>
-        <div class="bg-white hover:bg-gray-100 transition transform active:scale-95 duration-150 ease-in-out cursor-pointer rounded shadow-lg h-48">
-            <router-link to="/pokemon-add">
-                <img src="https://cdn.pixabay.com/photo/2021/12/26/17/31/pokemon-list-6895600_640.png" alt="Pokemon Placeholder" class="w-full h-3/4 object-contain ">
-                <p class="text-center mt-2">Pokemon Placeholder</p>
-            </router-link>
-        </div>
-        <div class="bg-white hover:bg-gray-100 transition transform active:scale-95 duration-150 ease-in-out cursor-pointer rounded shadow-lg h-48">
-            <router-link to="/pokemon-add">
-                <img src="https://cdn.pixabay.com/photo/2021/12/26/17/31/pokemon-list-6895600_640.png" alt="Pokemon Placeholder" class="w-full h-3/4 object-contain ">
-                <p class="text-center mt-2">Pokemon Placeholder</p>
-            </router-link>
-        </div>
-        <div class="bg-white hover:bg-gray-100 transition transform active:scale-95 duration-150 ease-in-out cursor-pointer rounded shadow-lg h-48">
-            <router-link to="/pokemon-add">
-                <img src="https://cdn.pixabay.com/photo/2021/12/26/17/31/pokemon-list-6895600_640.png" alt="Pokemon Placeholder" class="w-full h-3/4 object-contain ">
-                <p class="text-center mt-2">Pokemon Placeholder</p>
-            </router-link>
-        </div>
-        <div class="bg-white hover:bg-gray-100 transition transform active:scale-95 duration-150 ease-in-out cursor-pointer rounded shadow-lg h-48">
-            <router-link to="/pokemon-add">
-                <img src="https://cdn.pixabay.com/photo/2021/12/26/17/31/pokemon-list-6895600_640.png" alt="Pokemon Placeholder" class="w-full h-3/4 object-contain ">
-                <p class="text-center mt-2">Pokemon Placeholder</p>
-            </router-link>
-        </div>
-        <div class="bg-white hover:bg-gray-100 transition transform active:scale-95 duration-150 ease-in-out cursor-pointer rounded shadow-lg h-48">
-            <router-link to="/pokemon-add">
-                <img src="https://cdn.pixabay.com/photo/2021/12/26/17/31/pokemon-list-6895600_640.png" alt="Pokemon Placeholder" class="w-full h-3/4 object-contain ">
-                <p class="text-center mt-2">Pokemon Placeholder</p>
-            </router-link>
-        </div>
-        <div class="bg-white hover:bg-gray-100 transition transform active:scale-95 duration-150 ease-in-out cursor-pointer rounded shadow-lg h-48">
-            <router-link to="/pokemon-add">
-                <img src="https://cdn.pixabay.com/photo/2021/12/26/17/31/pokemon-list-6895600_640.png" alt="Pokemon Placeholder" class="w-full h-3/4 object-contain ">
-                <p class="text-center mt-2">Pokemon Placeholder</p>
-            </router-link>
-        </div>
-        <div class="bg-white hover:bg-gray-100 transition transform active:scale-95 duration-150 ease-in-out cursor-pointer rounded shadow-lg h-48">
-            <router-link to="/pokemon-add">
-                <img src="https://cdn.pixabay.com/photo/2021/12/26/17/31/pokemon-list-6895600_640.png" alt="Pokemon Placeholder" class="w-full h-3/4 object-contain ">
-                <p class="text-center mt-2">Pokemon Placeholder</p>
-            </router-link>
-        </div>
+      <div 
+        v-for="pokemon in pokemons"
+        :key="pokemon.name"
+        class="bg-white hover:bg-gray-100 transition transform active:scale-95 duration-150 ease-in-out cursor-pointer rounded shadow-lg h-48"
+      >
+        <router-link to="/pokemon-add">
+          <img :src="pokemon.image || 'https://cdn-icons-png.flaticon.com/512/419/419467.png'" :alt="pokemon.name" class="w-full h-3/4 object-contain" />
+          <p class="text-center mt-2 capitalize">{{ pokemon.name }}</p>
+        </router-link>
+      </div>
+    </div>
+
+    <!-- Pagination -->
+    <div >
+        <Pagination
+        :current-page="currentPage"
+        :total-pages="totalPages"
+        @change="goToPage"
+        />
     </div>
   </MainLayout>
 </template>
 
 <script setup>
+import { ref, onMounted, computed } from 'vue'
 import MainLayout from '../layouts/MainLayout.vue'
+import Pagination from '../components/Pagination.vue'
+import { fetchPokemonList } from '../services/pokemonListService'
+
+const pokemons = ref([])
+const totalCount = ref(0)
+const currentPage = ref(1)
+const pageSize = 20
+const totalPages = computed(() => Math.ceil(totalCount.value / pageSize))
+
+const loadPokemons = async () => {
+  const offset = (currentPage.value - 1) * pageSize
+  try {
+    const { pokemons: list, count } = await fetchPokemonList(pageSize, offset)
+    pokemons.value = list
+    totalCount.value = count
+  } catch (error) {
+    console.error('Could not load Pokémon:', error)
+  }
+}
+
+const goToPage = (page) => {
+  if (page >= 1 && page <= totalPages.value) {
+    currentPage.value = page
+    loadPokemons()
+  }
+}
+
+onMounted(loadPokemons)
 </script>
