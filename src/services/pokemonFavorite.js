7fec88d0b073bdcbb986222015f83604ef7d146d
KomangArmawan <komang.armawan.27@gmail.com>
2025-05-26 01:05:06 +0700

    update: fixing pagination on the home view

--- src/services/pokemonFavorite.js
+++ src/services/pokemonFavorite.js
@@ -2,6 +2,34 @@ import axios from 'axios'
 
 const BASE_URL = 'https://gopoke-production.up.railway.app/api/v1'
 
+// get
+export async function fetchFavoritePokemons(token, page = 1, limit = 20) {
+  try {
+    const res = await fetch(`${BASE_URL}/pokemons?page=${page}&limit=${limit}`, {
+      headers: {
+        Accept: 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    })
+
+    const json = await res.json()
+    if (json.success) {
+      return {
+        items: json.data.items,
+        currentPage: json.data.currentPage,
+        totalPages: json.data.totalPages,
+      }
+    } else {
+      throw new Error(json.message)
+    }
+  } catch (error) {
+    console.error('Error fetching favorite Pokémons:', error)
+    throw error
+  }
+}
+
+
+// create
 export async function createFavoritePokemon({ name, notes, sprite, type, userEmail }) {
   const token = localStorage.getItem('token') 
 
@@ -33,7 +61,7 @@ export async function createFavoritePokemon({ name, notes, sprite, type, userEma
   }
 }
 
-// services/pokemonFavorite.js
+// update
 export async function updateFavoritePokemon(id, data) {
   const token = localStorage.getItem('token')
   if (!token) throw new Error('User is not authenticated')
