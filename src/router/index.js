a10aa90a149727de1896cb32db60dbc9fb92e9f8
KomangArmawan <komang.armawan.27@gmail.com>
2025-05-25 13:55:09 +0700

    update: finishing oauth

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,40 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Login from '../views/Login.vue'
+import Register from '../views/Register.vue'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
-import Dashboard from '../views/Dashboard.vue'
+// import Dashboard from '../views/Dashboard.vue'
 import PokemonList from '../views/PokemonList.vue'
 import AddPokemon from '../views/AddPokemon.vue'
 
 const routes = [
-  { path: '/', name: 'Home', component: Home },
-  { path: '/about', name: 'About', component: About },
-  { path: '/dashboard', name: 'Dashboard', component: Dashboard },
-  { path: '/pokemon-list', name: 'PokemonList', component: PokemonList },
-  { path: '/pokemon-add', name: 'AddPokemon', component: AddPokemon },
+  // authentication routes
+  { path: '/login', name: 'Login', component: Login },
+  { path: '/register', name: 'Register', component: Register },
+
+  // main application routes
+  { path: '/', name: 'Home', component: Home, meta: { requiresAuth: true }},
+  { path: '/about', name: 'About', component: About, meta: { requiresAuth: true }},
+  // { path: '/dashboard', name: 'Dashboard', component: Dashboard, meta: { requiresAuth: true }},
+  { path: '/pokemon-list', name: 'PokemonList', component: PokemonList, meta: { requiresAuth: true }},
+  { path: '/pokemon-add', name: 'AddPokemon', component: AddPokemon, meta: { requiresAuth: true }},
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+})
+
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem('token')
+
+  if (to.meta.requiresAuth && !token) {
+    next({ name: 'Login' })
+  } else if ((to.name === 'Login' || to.name === 'Register') && token) {
+    next({ name: 'Home' })
+  } else {
+    next()
+  }
 })
 
 export default router
